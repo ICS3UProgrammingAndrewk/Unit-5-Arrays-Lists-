31406cf24b7aed4178098f2c83cba2dad47d293f
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-06-11 15:14:18 -0400

    Add files via upload

---
+++ IntroductionToArraysAndrew/IntroductionToArraysAndrew/Introduction to ArraysForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace IntroductionToArraysAndrew
+{
+    partial class frmIntroductiontoArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculator = new System.Windows.Forms.Button();
+            this.IstArray = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(123, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 36);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(284, 279);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(24, 20);
+            this.lblAnswer.TabIndex = 1;
+            this.lblAnswer.Text = "...";
+            // 
+            // btnCalculator
+            // 
+            this.btnCalculator.BackColor = System.Drawing.Color.Yellow;
+            this.btnCalculator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculator.Location = new System.Drawing.Point(67, 266);
+            this.btnCalculator.Name = "btnCalculator";
+            this.btnCalculator.Size = new System.Drawing.Size(201, 36);
+            this.btnCalculator.TabIndex = 2;
+            this.btnCalculator.Text = "Calculate Averge";
+            this.btnCalculator.UseVisualStyleBackColor = false;
+            this.btnCalculator.Click += new System.EventHandler(this.btnCalculator_Click);
+            // 
+            // IstArray
+            // 
+            this.IstArray.FormattingEnabled = true;
+            this.IstArray.Location = new System.Drawing.Point(67, 87);
+            this.IstArray.Name = "IstArray";
+            this.IstArray.Size = new System.Drawing.Size(201, 160);
+            this.IstArray.TabIndex = 3;
+            // 
+            // frmIntroductiontoArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(408, 332);
+            this.Controls.Add(this.IstArray);
+            this.Controls.Add(this.btnCalculator);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmIntroductiontoArrays";
+            this.Text = "Introduction to Arrays By Andrew K.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculator;
+        private System.Windows.Forms.ListBox IstArray;
+    }
+}
+
---
+++ IntroductionToArraysAndrew/IntroductionToArraysAndrew/Introduction to ArraysForm.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Created by: Andrew Kuekam
+ * Created on: 2020-06-10
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Introduction to Arrays
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroductionToArraysAndrew
+{
+    public partial class frmIntroductiontoArrays : Form
+    {
+        // declare global vers and const
+        const int MAX_ARRAY_SIZE = 10;
+        int[] arrayOfNumbers = new int[MAX_ARRAY_SIZE];
+        public frmIntroductiontoArrays()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare locla vers
+            int randomNum;
+            int counter;
+            Random randomNumberGenerator = new Random();
+
+            // clear list box
+            this.IstArray.Items.Clear();
+
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                // generate a new random num beween 1 and 10
+                randomNum = randomNumberGenerator.Next(1, MAX_ARRAY_SIZE + 1 );
+
+                // assign the random num to the cell posistion in the array
+                arrayOfNumbers[counter] = randomNum;
+
+                // add number to the list box
+                this.IstArray.Items.Add(randomNum);
+
+                // refresh the from to display the new item
+                this.Refresh();
+
+            }
+
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            // declare local vars
+            double average = 0;
+            double sum = 0;
+            int counter;
+
+            // calculate the sum pf all array values
+            for (counter = 0; counter < arrayOfNumbers.Length; counter++)
+            {
+                sum = sum + arrayOfNumbers[counter];
+            }
+            // calculate the averge of the value in the array
+            average = sum / arrayOfNumbers.Length;
+
+            // display the average in the label
+            this.lblAnswer.Text = " Average: " + average;
+
+        }
+    }
+}
